7962624d4907298ff23aa0bc7dc4521a78c3a65d
Matt Weber <matt@badecho.com>
2025-01-05 16:49:44 -0600

    Adding required build assets

--- src/Extensibility/Properties/AssemblyInfo.cs
+++ src/Extensibility/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 #if RELEASE
-using BadEcho.Extensiblity.Properties;
+using BadEcho.Extensibility.Properties;
 
 [assembly: InternalsVisibleTo("BadEcho.Extensibility.Tests,PublicKey="+BuildInfo.PublicKey)]
 #else
